9b9fcfe72cfa9763f4b793df247857790dda34dc
pedrobn <pedrobonillanadal@gmail.com>
2022-06-13 01:09:23 +0200

    edit

--- experiments/__main__.py
+++ experiments/__main__.py
@@ -1,23 +1,25 @@
 import dataclasses
 import os
 import numpy as np
+import pandas as pd
 
 from pgmpy.factors.discrete import CPD
 from experiments import approximations, bounds, read
 from typing import List
 
 PATH = 'networks'
-NETWORKS = os.listdir(PATH)
+#NETWORKS = os.listdir(PATH)
+NETWORKS = ['alarm.bif']
 
 
 @dataclasses.dataclass
 class Result:
-    variable : str
-    net : str
-    original_DKL_error : float
-    original_L1_error : float
-    M_error : float
-    D_error : float
+    variable: str
+    net: str
+    original_DKL_error: float
+    original_L1_error: float
+    M_error: float
+    D_error: float
 
     @classmethod
     def from_dict(cls, dict_: dict):
@@ -39,40 +41,35 @@ class Result:
         return dataclasses.astuple(self)
 
     def __str__(self):
-        return(f'- Error results for variable {self.variable} in net {net}:\n'
-               f'    * correct DKL: {self.original_DKL_error}\n' 
-               f'    * correct L1: {self.original_L1_error}\n' 
-               f'    * M bound: {self.M_error}\n' 
-               f'    * D bound: {self.D_error}.' )
+        return (f'- Error results for variable {self.variable} in net {net}:\n'
+                f'    * correct DKL: {self.original_DKL_error}\n'
+                f'    * correct L1: {self.original_L1_error}\n'
+                f'    * M bound: {self.M_error}\n'
+                f'    * D bound: {self.D_error}.')
+
 
 if __name__ == "__main__":
     results = []
     for l1_error in [0.01, 0.05, 0.1]:
         for net, cpds in read.nets(PATH, NETWORKS):
-            print()
+            results = []
             print(f'--- Approximating net: {net}')
             approx_cpds = (approximations.noise(cpd) for cpd in cpds)
             print('------> done')
             for cpd, approx_cpd in zip(cpds, approx_cpds):
 
-
-                others = [other for other in cpds if other != cpd] 
+                print(cpd)
+                others = [other for other in cpds if other != cpd]
 
                 res = Result(
-                    variable = cpd.variable,
-                    net = net,
-                    original_DKL_error = bounds.kl_divergence(cpd, approx_cpd),
-                    original_L1_error = bounds.l1_distance(cpd, approx_cpd),
-                    M_error = bounds.M_error(cpd, approx_cpd, *others),
-                    D_error = bounds.D_error(cpd, approx_cpd, *others),
+                    variable=cpd.variable,
+                    net=net[:-4],
+                    original_DKL_error=bounds.kl_divergence(cpd, approx_cpd),
+                    original_L1_error=bounds.l1_distance(cpd, approx_cpd),
+                    M_error=bounds.M_error(cpd, approx_cpd, *others),
+                    D_error=bounds.D_error(cpd, approx_cpd, *others),
                 )
-
-
-                print()
-                print(res)
-
                 results.append(res)
-            
-        
-    print(results)
-    
+
+            df = pd.DataFrame.from_dict([result.asdict() for result in results])
+            df.to_csv(f'{net[:-4]}.csv', index=False)
--- experiments/bounds.py
+++ experiments/bounds.py
@@ -3,61 +3,76 @@ from pgmpy.factors.discrete import CPD
 from scipy import stats, special
 
 
-
 def log_values(cpd, approx_cpd):
     values = cpd.values
     approx_values = approx_cpd.values
 
-    log_values = special.rel_entr(values.flatten(),approx_values.flatten())
+    log_values = special.rel_entr(values.flatten(), approx_values.flatten())
     return log_values.reshape(values.shape)
 
 
 def l1_distance(cpd, approx_cpd):
-    return abs(cpd.values-approx_cpd.values).sum()
+    return abs(cpd.values - approx_cpd.values).sum()
+
 
 def kl_divergence(cpd, approx_cpd):
     return log_values(cpd, approx_cpd).sum()
 
+
 def marg_and_max(values, variables, marginalized):
     # Marg
-    axis = tuple(variables.index(var) for var in marginalized if var in variables)
+    axis = tuple(
+        variables.index(var) for var in marginalized if var in variables)
     logvalues = values.sum(axis=axis)
 
     # Max
     return values.max()
-    
+
 
 def M_error(cpd, approx_cpd, *others):
-    other_vars = {var for other in others for var in other.variables if var in cpd.variables}
+    other_vars = {
+        var for other in others for var in other.variables
+        if var in cpd.variables
+    }
     logvalues = log_values(cpd, approx_cpd)
 
     result = marg_and_max(logvalues, cpd.variables, other_vars)
-    normalization_factor = cpd.values.min()
 
-    return result/normalization_factor
+    if (normalization_factor := cpd.values.min()) != 0:
+        return result / normalization_factor
+    else:
+        return float('inf')
+
 
 def poduct_M_bound(cpds, approx_cpds):
+
     def others(cpds, cpd):
         others = cpds[:]
         others.remove(cpd)
-    
-    return sum(M_error(cpd,
-                       approx_cpd,
-                       others(cpds, cpd)) for cpd, approx_cpd in zip(cpds, approx_cds))
+
+    return sum(
+        M_error(cpd, approx_cpd, others(cpds, cpd))
+        for cpd, approx_cpd in zip(cpds, approx_cds))
+
 
 def relative_weight(weight_cpd, object_cpd):
-    return marg_and_max(weight_cpd.values, weight_cpd.variables, object_cpd.variables)
+    return marg_and_max(weight_cpd.values, weight_cpd.variables,
+                        object_cpd.variables)
+
 
 def D_error(cpd, approx_cpd, *others):
-    l1_error = abs(cpd.values-approx_cpd.values).sum()
-    D_KL =  special.rel_entr(cpd.values.flatten(),approx_cpd.values.flatten()).sum()
+    l1_error = abs(cpd.values - approx_cpd.values).sum()
+    D_KL = special.rel_entr(cpd.values.flatten(),
+                            approx_cpd.values.flatten()).sum()
 
-    return np.prod([relative_weight(other, cpd)  for other in others])* (l1_error + D_KL)
+    return np.prod([relative_weight(other, cpd) for other in others
+                   ]) * (l1_error + D_KL)
+
 
 def poduct_D_bound(cpds, approx_cpds, goal_weight):
-    result = sum(M_error(cpd,
-                         approx_cpd,
-                         others(cpds, cpd)) for cpd, approx_cpd in zip(cpds, approx_cds))
+    result = sum(
+        M_error(cpd, approx_cpd, others(cpds, cpd))
+        for cpd, approx_cpd in zip(cpds, approx_cds))
 
     normalization_factor = goal_weight
-    return result/normalization_factor
+    return result / normalization_factor
